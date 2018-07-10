8459a569f1d3c3d14624e653e14c440d6f3343fb
isa <isa>
2018-07-11 00:40:29 +0900

    イテレータパターンFIX
    t/o

--- designPattern/designPattern/Program.cs
+++ designPattern/designPattern/Program.cs
@@ -10,11 +10,104 @@ namespace designPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BookShelf bookShelf = new BookShelf(4);
+            bookShelf.appendBook(new Book("book1aa"));
+            bookShelf.appendBook(new Book("book2b"));
+            bookShelf.appendBook(new Book("book3ss"));
+            bookShelf.appendBook(new Book("book4___"));
 
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
+            IIterator it = bookShelf.iterator();
+            Book book;
+            while (it.hasNext())
+            {
+                book = (Book)it.next();
+                Console.WriteLine(book.name);
+            }
             Console.ReadKey();
+
         }
     }
+
+    interface IIterator
+    {
+        bool hasNext();
+        Object next();
+    }
+
+    interface IAggregate
+    {
+        IIterator iterator();
+    }
+
+    class Book
+    {
+        public Book(string name_)
+        {
+            this.name = name_;
+        }
+
+        public string name { get; private set; }
+    }
+
+    class BookShelf : IAggregate
+    {
+        public BookShelf(int maxSize_)
+        {
+            _last  = 0;
+            _books = new Book[ maxSize_ ];
+        }
+
+        public Book getBookAt(int index_)
+        {
+            return _books[index_];
+        }
+
+        public void appendBook(Book book_)
+        {
+            _books[_last] = book_;
+            _last++;
+        }
+
+        public int getLength()
+        {
+            return _last;
+        }
+
+        public IIterator iterator()
+        {
+            return new BookShelfIterator(this);
+        }
+
+        int _last;
+        Book[] _books;
+    }
+
+    class BookShelfIterator : IIterator
+    {
+        public BookShelfIterator(BookShelf bookShelf_)
+        {
+            _bookShelf = bookShelf_;
+            _index = 0;
+        }
+
+        public bool hasNext()
+        {
+            return (_index < _bookShelf.getLength());
+        }
+
+        public Object next()
+        {
+            Book book = _bookShelf.getBookAt(_index);
+            _index++;
+            return book;
+        }
+
+        BookShelf _bookShelf;
+        int _index;
+    }
+
+
+
+
+
 }
